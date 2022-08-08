d64dce034a928c2f8d2e56ca04655b3bcb1d25f6
Reynaldo <pr19004@ues.edu.sv>
2022-08-08 15:48:23 -0600

    Disenio del login, registrarse terminado

--- app/src/main/java/com/example/app_restaurant/MainActivity.java
+++ app/src/main/java/com/example/app_restaurant/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.app_restaurant;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
+
+
+    /*Metodo o evento clic en el boton de iniciar sesion en la app*/
+    public void clicIniciarSesion(View view){
+
+        startActivity(new Intent(MainActivity.this,Login.class));
+        //finish();
+
+    }
+
+    /*Metodo o evento clic en el boton de Registrarse en la app*/
+    public void clicRegistrarse(View view){
+
+        startActivity(new Intent(MainActivity.this,RegisterActivity.class));
+
+    }
+
 }
